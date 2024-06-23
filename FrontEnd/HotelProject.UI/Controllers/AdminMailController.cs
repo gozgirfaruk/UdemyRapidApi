618f286c97da3d68bb6f2b74c84ee8c1e5e11fea
gozgirfaruk <gozgirfaruk@gmail.com>
2024-06-23 13:57:26 +0300

    Mail gönderim ve dosya işlemleri gerçekleştirildi

---
+++ FrontEnd/HotelProject.UI/Controllers/AdminMailController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.UI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.UI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminmailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddress = new MailboxAddress("HotelierAdmin", "gozegir127@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+            mimeMessage.Subject=model.Subject;
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody=model.Body;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("gozegir127@gmail.com", "pfbldjuxjiah skec");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+
+            return RedirectToAction("Sendbox", "AdminContract");
+        }
+    }
+}
